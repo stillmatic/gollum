decc3b6b27666e56f81aedd13d63f627b675f2a0
Chris Hua <hua.christopher@gmail.com>
2023-07-14 09:34:03 -0600

    fix: key logic error, more realistic benchmarks

--- vectorstore_compressed.go
+++ vectorstore_compressed.go
@@ -14,17 +14,21 @@ import (
 // Compressor is a single method interface that returns a compressed representation of an object.
 type Compressor interface {
 	Compress(src []byte) []byte
+	CompressIO(in io.Reader, out io.Writer) error
 }
 
 // GzipCompressor uses the klauspost/compress gzip compressor.
 // We generally suggest using this optimized implementation over the stdlib.
 type GzipCompressor struct {
-	pool syncpool.Pool[*gzip.Writer]
+	pool    syncpool.Pool[*gzip.Writer]
+	bufPool syncpool.Pool[*bytes.Buffer]
 }
 
 // ZstdCompressor uses the klauspost/compress zstd compressor.
 type ZstdCompressor struct {
-	pool syncpool.Pool[*zstd.Encoder]
+	pool    syncpool.Pool[*zstd.Encoder]
+	bufPool syncpool.Pool[*bytes.Buffer]
+	enc     *zstd.Encoder
 }
 
 // StdGzipCompressor uses the std gzip compressor.
@@ -33,35 +37,67 @@ type StdGzipCompressor struct {
 }
 
 func (g *GzipCompressor) Compress(src []byte) []byte {
-	w := io.Discard
 	var b bytes.Buffer
 	gz := g.pool.Get()
-	gz.Reset(w)
+	defer g.pool.Put(gz)
 
+	gz.Reset(&b)
 	if _, err := gz.Write(src); err != nil {
 		panic(err)
 	}
-	if err := gz.Flush(); err != nil {
+	if err := gz.Close(); err != nil {
 		panic(err)
 	}
-	g.pool.Put(gz)
 	return b.Bytes()
 }
 
-func (g *ZstdCompressor) Compress(src []byte) []byte {
-	w := io.Discard
+func (g *GzipCompressor) CompressIO(in io.Reader, out io.Writer) error {
+	enc := g.pool.Get()
+	defer g.pool.Put(enc)
 	var b bytes.Buffer
-	zstd := g.pool.Get()
-	zstd.Reset(w)
+	enc.Reset(&b)
 
+	if _, err := io.Copy(enc, in); err != nil {
+		return err
+	}
+	if err := enc.Flush(); err != nil {
+		return err
+	}
+	return enc.Close()
+}
+
+func (g *ZstdCompressor) Compress(src []byte) []byte {
+	return g.enc.EncodeAll(src, make([]byte, 0, len(src)))
+
+	var b bytes.Buffer
+	zstd := g.enc
+	// zstd := g.pool.Get()
+	zstd.Reset(&b)
 	if _, err := zstd.Write(src); err != nil {
 		panic(err)
 	}
-	if err := zstd.Close(); err != nil {
+	if err := zstd.Flush(); err != nil {
 		panic(err)
 	}
-	g.pool.Put(zstd)
+
+	// g.pool.Put(zstd)
 	return b.Bytes()
+}
+
+func (g *ZstdCompressor) CompressIO(in io.Reader, out io.Writer) error {
+	enc := g.enc
+	// enc := g.pool.Get()
+	// defer g.pool.Put(enc)
+	var b bytes.Buffer
+	enc.Reset(&b)
+
+	if _, err := io.Copy(enc, in); err != nil {
+		return err
+	}
+	if err := enc.Flush(); err != nil {
+		return err
+	}
+	return enc.Close()
 }
 
 func (g *StdGzipCompressor) Compress(src []byte) []byte {
@@ -78,6 +114,21 @@ func (g *StdGzipCompressor) Compress(src []byte) []byte {
 	}
 	g.pool.Put(gz)
 	return b.Bytes()
+}
+
+func (g *StdGzipCompressor) CompressIO(in io.Reader, out io.Writer) error {
+	enc := g.pool.Get()
+	defer g.pool.Put(enc)
+	var b bytes.Buffer
+	enc.Reset(&b)
+
+	if _, err := io.Copy(enc, in); err != nil {
+		return err
+	}
+	if err := enc.Flush(); err != nil {
+		return err
+	}
+	return enc.Close()
 }
 
 type CompressedDocument struct {
@@ -98,7 +149,7 @@ func (ts *CompressedVectorStore) Insert(ctx context.Context, d Document) error {
 	return nil
 }
 
-func minMax(val1, val2 int) (int, int) {
+func minMax(val1, val2 float64) (float64, float64) {
 	if val1 < val2 {
 		return val1, val2
 	}
@@ -107,21 +158,40 @@ func minMax(val1, val2 int) (int, int) {
 
 func (cvs *CompressedVectorStore) Query(ctx context.Context, qb QueryRequest) ([]Document, error) {
 	// singlethreaded approach
+	// out := &bytes.Buffer{}
+	// qbytes := bytes.NewReader([]byte(qb.Query))
+	// err := cvs.Compressor.CompressIO(qbytes, out)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// searchTermEncoded := out.Bytes()
 	searchTermEncoded := cvs.Compressor.Compress([]byte(qb.Query))
 
 	h := Heap{}
 	h.Init()
 	k := qb.K
+	if k > len(cvs.Data) {
+		k = len(cvs.Data)
+	}
 
 	for _, doc := range cvs.Data {
-		Cx1x2 := len(cvs.Compressor.Compress(append(searchTermEncoded, doc.Encoded...)))
-		min, max := minMax(len(searchTermEncoded), len(doc.Encoded))
-		ncd := float32(Cx1x2-min) / float32(max)
+		Cx1 := float64(len(searchTermEncoded))
+		Cx2 := float64(len(doc.Encoded))
+		// x1x2 := bytes.NewReader([]byte(qb.Query + " " + doc.Content))
+		x1x2 := cvs.Compressor.Compress([]byte(qb.Query + " " + doc.Content))
+		// out.Reset()
+		// err = cvs.Compressor.CompressIO(x1x2, out)
+		// if err != nil {
+		// 	return nil, err
+		// }
+		// Cx1x2 := float64(len(out.Bytes()))
+		Cx1x2 := float64(len(x1x2))
+		min, max := minMax(Cx1, Cx2)
+		ncd := (Cx1x2 - min) / (max)
 
-		// We want a max heap, so we take the negative of ncd
 		node := nodeSimilarity{
 			Document:   doc.Document,
-			Similarity: -ncd,
+			Similarity: float32(ncd),
 		}
 
 		h.Push(node)
@@ -214,10 +284,15 @@ func NewGzipVectorStore() *CompressedVectorStore {
 
 func NewZstdVectorStore() *CompressedVectorStore {
 	w := io.Discard
+	enc, err := zstd.NewWriter(w, zstd.WithEncoderCRC(false))
+	if err != nil {
+		panic(err)
+	}
 	return &CompressedVectorStore{
 		Compressor: &ZstdCompressor{
+			enc: enc,
 			pool: syncpool.New[*zstd.Encoder](func() *zstd.Encoder {
-				enc, err := zstd.NewWriter(w)
+				enc, err := zstd.NewWriter(w, zstd.WithEncoderCRC(false))
 				if err != nil {
 					panic(err)
 				}
--- vectorstore_compressed_test.go
+++ vectorstore_compressed_test.go
@@ -1,12 +1,14 @@
 package gollum_test
 
 import (
+	"bufio"
 	"context"
 	"crypto/rand"
 	"fmt"
 	mathrand "math/rand"
-	"sync"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/stillmatic/gollum"
@@ -14,7 +16,7 @@ import (
 )
 
 func TestCompressedVectorStore(t *testing.T) {
-	vs := gollum.NewStdGzipVectorStore()
+	vs := gollum.NewGzipVectorStore()
 	t.Run("implements interface", func(t *testing.T) {
 		var vs2 gollum.VectorStore
 		vs2 = vs
@@ -22,11 +24,9 @@ func TestCompressedVectorStore(t *testing.T) {
 	})
 	ctx := context.Background()
 	testStrings := []string{
-		"apple",
-		"orange",
-		"basketball",
-		"football",
-		"banana",
+		"Japan's Seiko Epson Corp. has developed a 12-gram flying microrobot.",
+		"The latest tiny flying robot has been unveiled in Japan.",
+		"Michael Phelps won the gold medal in the 400 individual medley.",
 	}
 	t.Run("testInsert", func(t *testing.T) {
 		for _, str := range testStrings {
@@ -47,17 +47,17 @@ func TestCompressedVectorStore(t *testing.T) {
 			})
 		}
 		docs, err := vs.Query(ctx, gollum.QueryRequest{
-			Query: "tennis ball",
+			Query: "Where was the new robot unveiled?",
 			K:     5,
 		})
 		assert.NoError(t, err)
-		assert.Equal(t, 5, len(docs))
-		assert.Equal(t, "football", docs[0].Content)
-		assert.Equal(t, "basketball", docs[1].Content)
+		assert.Equal(t, 3, len(docs))
+		assert.Equal(t, "Japan's Seiko Epson Corp. has developed a 12-gram flying microrobot.", docs[0].Content)
+		assert.Equal(t, "The latest tiny flying robot has been unveiled in Japan.", docs[1].Content)
 	})
 }
 
-func BenchmarkStdGzipCompressedVectorStore(b *testing.B) {
+func BenchmarkCompressedVectorStore(b *testing.B) {
 	ctx := context.Background()
 	// Test different sizes
 	sizes := []int{10, 100, 1000, 10_000, 100_000}
@@ -65,75 +65,89 @@ func BenchmarkStdGzipCompressedVectorStore(b *testing.B) {
 	ks := []int{1, 10, 100}
 	// benchmark inserts
 	stores := map[string]gollum.VectorStore{
-		// "StdGzipVectorStore": gollum.NewStdGzipVectorStore(),
-		"ZstdVectorStore": gollum.NewZstdVectorStore(),
-		// "GzipVectorStore":    gollum.NewGzipVectorStore(),
+		"StdGzipVectorStore": gollum.NewStdGzipVectorStore(),
+		"ZstdVectorStore":    gollum.NewZstdVectorStore(),
+		"GzipVectorStore":    gollum.NewGzipVectorStore(),
 	}
 
 	for vsName, vs := range stores {
-		for _, size := range sizes {
-			b.Run(fmt.Sprintf("%s-Insert-%d", vsName, size), func(b *testing.B) {
-				// Create vector store using live compression
-				docs := make([]gollum.Document, size)
-				// Generate synthetic docs
-				for i := range docs {
-					docs[i] = syntheticDoc()
-				}
-				b.ReportAllocs()
-				b.ResetTimer()
-				for n := 0; n < b.N; n++ {
-					// Insert docs
-					for _, doc := range docs {
-						vs.Insert(ctx, doc)
-					}
-				}
-			})
-		}
-		// Concurrent writes to a slice are ok
-		for _, size := range sizes {
-			b.Run(fmt.Sprintf("%s-InsertConcurrent-%d", vsName, size), func(b *testing.B) {
-				// Create vector store using live compression
-				vs := gollum.NewStdGzipVectorStore()
-				docs := make([]gollum.Document, size)
-				// Generate synthetic docs
-				for i := range docs {
-					docs[i] = syntheticDoc()
-				}
-				var wg sync.WaitGroup
-				sem := make(chan struct{}, 8)
-				b.ReportAllocs()
-				b.ResetTimer()
-				for n := 0; n < b.N; n++ {
-					// Insert docs
-					for _, doc := range docs {
-						wg.Add(1)
-						sem <- struct{}{}
-						go func(doc gollum.Document) {
-							defer wg.Done()
-							defer func() { <-sem }()
-							vs.Insert(ctx, doc)
-						}(doc)
-					}
-					wg.Wait()
-				}
-			})
-		}
+		// for _, size := range sizes {
+		// 	b.Run(fmt.Sprintf("%s-Insert-%d", vsName, size), func(b *testing.B) {
+		// 		// Create vector store using live compression
+		// 		docs := make([]gollum.Document, size)
+		// 		// Generate synthetic docs
+		// 		for i := range docs {
+		// 			docs[i] = syntheticDoc()
+		// 		}
+		// 		b.ReportAllocs()
+		// 		b.ResetTimer()
+		// 		for n := 0; n < b.N; n++ {
+		// 			// Insert docs
+		// 			for _, doc := range docs {
+		// 				vs.Insert(ctx, doc)
+		// 			}
+		// 		}
+		// 	})
+		// }
+		// // Concurrent writes to a slice are ok
+		// for _, size := range sizes {
+		// 	b.Run(fmt.Sprintf("%s-InsertConcurrent-%d", vsName, size), func(b *testing.B) {
+		// 		// Create vector store using live compression
+		// 		docs := make([]gollum.Document, size)
+		// 		// Generate synthetic docs
+		// 		for i := range docs {
+		// 			docs[i] = syntheticDoc()
+		// 		}
+		// 		var wg sync.WaitGroup
+		// 		sem := make(chan struct{}, 8)
+		// 		b.ReportAllocs()
+		// 		b.ResetTimer()
+		// 		for n := 0; n < b.N; n++ {
+		// 			// Insert docs
+		// 			for _, doc := range docs {
+		// 				wg.Add(1)
+		// 				sem <- struct{}{}
+		// 				go func(doc gollum.Document) {
+		// 					defer wg.Done()
+		// 					defer func() { <-sem }()
+		// 					vs.Insert(ctx, doc)
+		// 				}(doc)
+		// 			}
+		// 			wg.Wait()
+		// 		}
+		// 	})
+		// }
 		// benchmark queries
 		for _, size := range sizes {
+			f, err := os.Open("testdata/enwik8")
+			if err != nil {
+				panic(err)
+			}
+			defer f.Close()
+			lines := make([]string, 0)
+			scanner := bufio.NewScanner(f)
+			for scanner.Scan() {
+				lines = append(lines, scanner.Text())
+			}
 			for _, k := range ks {
 				if k <= size {
 					b.Run(fmt.Sprintf("%s-Query-%d-%d", vsName, size, k), func(b *testing.B) {
 						// Create vector store and insert docs
-						vs := gollum.NewStdGzipVectorStore()
-						docs := make([]gollum.Document, size)
-						for i := range docs {
-							docs[i] = syntheticDoc()
-							vs.Insert(ctx, docs[i])
+						randSource := mathrand.NewSource(time.Now().UnixNano())
+						mathrand := mathrand.New(randSource)
+						// Seed random number generator
+						mathrand.Seed(time.Now().UnixNano())
+						// Seed random number generator
+						mathrand.Seed(time.Now().UnixNano())
+
+						for i := 0; i < size; i++ {
+							randIndex := mathrand.Intn(len(lines))
+							vs.Insert(ctx, gollum.NewDocumentFromString(lines[randIndex]))
 						}
+						query := syntheticQuery(k)
 						b.ReportAllocs()
 						b.ResetTimer()
 						// Create query
-						query := syntheticQuery(k)
 						for n := 0; n < b.N; n++ {
 							vs.Query(ctx, query)
 						}
@@ -146,8 +160,8 @@ func BenchmarkStdGzipCompressedVectorStore(b *testing.B) {
 
 // Helper functions
 func syntheticString() string {
-	// Random length between 8 and 64
-	randLength := mathrand.Intn(64-8+1) + 8
+	// Random length between 8 and 32
+	randLength := mathrand.Intn(32-8+1) + 8
 
 	// Generate random bytes
 	randBytes := make([]byte, randLength)
